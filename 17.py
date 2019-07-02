4338652dd51b2ef1a0ee506c38a89d924a4859ca
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-07-02 11:18:34 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -2,11 +2,11 @@ n=int(input())
 h=0
 a=n
 while a>0:
- digit =a%10
- h +=digit**3
- a //=10
+   digit=a%10
+   h +=digit**3
+   a//=10
 if n==h:
- print("yes")
+   print("yes")
 else:
- print("no")
+   print("no")
   
