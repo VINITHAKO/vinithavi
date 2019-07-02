653df45836a8b770c872183def38420d4186ffb8
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-07-02 11:23:17 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,8 +1,8 @@
-n,t=map(int,input().split())
-for num in range(n,t+1):
- if num>1:
-   for i in range(2,num):
-    if(num%i)==0:
-      break
-   else:
-    print(num,end="")
+v,i=map(int,input().split())
+for k in range (v+1,i+1):
+  if(k>1):
+    for l in range (2,k):
+      if(k%l==0):
+        break
+    else:
+      print(k,end=" ")
